6909f8ff880b399a88981c57f000c8902f177a81
andre806 <megagamescanal6@gmail.com>
2025-07-21 20:44:36 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval'; object-src 'none';",
-          },
-        ],
-      },
-    ];
-  },
-};
+
